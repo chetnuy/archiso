d006e4002ab1a8fab0404f9660bfd92d8e14bb1f
chetnuy <chetnuy@gmail.com>
2018-05-27 01:23:30 +0300

    complete local commit

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 My setting archiso operate scripts
 =====================================
 For gentoo system. See pacman.conf file
+
+## Add function
+
+locale.conf
+locale.gen
+vconsole.conf
+change localtime
+usermod -s /bin/bash
+
+
+## Todo
+password for sshd
+add .bashrc
+add .local/ranger
+install yaourt
+install megatools
+
+...many-many
+
--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -2,12 +2,13 @@
 
 set -e -u
 
-sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+#sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(ru_RU\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
-usermod -s /usr/bin/zsh root
+usermod -s /usr/bin/bash root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
@@ -19,5 +20,5 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service choose-mirror.service
+systemctl enable pacman-init.service choose-mirror.service sshd.service
 systemctl set-default multi-user.target
